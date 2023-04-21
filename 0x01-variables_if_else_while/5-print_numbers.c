e75d39b317286c078942e1d6c3225a7d76687a14
Flora Frimpong-Boateng <florafrimps@gmail.com>
2023-04-21 13:10:22 -0700

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Single digit numbers of base 10
+ * main - Prints all single digit numbers of base 10 starting from 0
  *
  * Return: Always 0 (Success)
  */
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
+
 /**
- * main -Lower case alphabets in reverse
+ * main - prints all single digit numbers of base 10 starting from 0
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
     int i;
 
-    for (i = 0; i < 10; i++) {
+    for (i = 0; i < 10; i++)
+    {
         putchar(i + '0');
     }
 
     putchar('\n');
-    return 0;
+
+    return (0);
 }
