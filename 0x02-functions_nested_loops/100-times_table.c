eb6cbd24ab531fd8db64bee51577bfc8ca533769
Flora Frimpong-Boateng <florafrimps@gmail.com>
2023-04-26 12:52:15 -0700

    The World looks like a multiplication-table, or a mathematical equation

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,31 +7,29 @@
  */
 
 void print_times_table(int n)
-
 {
 
-int num, mult, prod;
-    
+int num, mult, prod;    
 if (n >= 0 && n <= 15)
 {
-for (num =0; num<= n; mult++)
+for (num =0; num <= n; mult++)
 {
 _putchar(0);
-for (mult =1; mult <= n; mult++)
+for (mult = 1; mult <= n; mult++)
 {
 _putchar(',');
 _putchar(' ');
 prod = num * mult;
-if (prod <= 99)
+if (prod < 99)
 _putchar(' ');
-if (prod <= 9)
+if (prod < 9)
 _putchar(' ');
-if (prod >= 100)
+if (prod > 100)
 {
 _putchar((prod / 100) + '0');
 _putchar(((prod / 100)) % 10 + '0');
 }
-else if (prod <= 99 && prod >= 10)
+else if (prod < 99 && prod > 10)
 {
 _putchar((prod / 10) + '0');
 }
