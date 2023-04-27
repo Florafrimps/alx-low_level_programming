01925f4ba40e59aa50003f9816fefc02b0122614
Flora Frimpong-Boateng <florafrimps@gmail.com>
2023-04-27 11:12:35 -0700

    Collaboration is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,7 +4,7 @@
  * mul - multiplies two integers
  * @a: first integer
  * @b: second integer
- * Return: the product of a and b
+ * Return: addition of the product of a and b
  */
 int mul(int a, int b)
 {
