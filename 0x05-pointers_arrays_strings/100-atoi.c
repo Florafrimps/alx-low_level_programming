d41424bb02882937e50e0418ed7a81219db5a6c9
Flora Frimpong-Boateng <florafrimps@gmail.com>
2023-05-03 11:04:34 -0700

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,44 +1,36 @@
 #include "main.h"
 
 /**
- * _atoi - convertsa string to an integer
+ * _atoi - converts a string to an integer
  * @s: string to be converted
  *
  * Return: the int converted from the string
  */
-
 int _atoi(char *s)
 {
-int i, d, n, len, f, digit;
+int i = 0, sign = 1, result = 0;
+while (s[i] == ' ')
+i++;
 
-i = 0;
-d = 0;
-n = 0;
-len = 0;
-f = 0;
-digit = 0;
-
-while (*s[len] != '\0')
-len++;
-while  (i < len && f == 0)
-{
-if (s[i] == '-')
-++d;
-if (s[i] >= '0' && s[i] <= '9')
-{
-digit = s[i] - '0';
-if (d % 2)
-digit = - digit;
-n = n * 10 + digit;
-f = 1;
-if (s[i + 1] < '0' || s[i + 1] > '9'
-}
-break;
-f = 0;
-}
+if (s[i] == '-') {
+sign = -1;
+i++;
+} else if (s[i] == '+') {
 i++;
 }
-if (f == 0)
-return (0);
-return (n);
+while (s[i] >= '0' && s[i] <= '9') {
+ 
+if (result > INT_MAX / 10 || (result == INT_MAX / 10 && s[i] - '0' > INT_MAX % 10)) {
+if (sign == 1) {
+return INT_MAX;
+} else {
+return INT_MIN;
 }
+}
+result = result * 10 + (s[i] - '0');
+i++;
+}
+
+return sign * result;
+}
+
