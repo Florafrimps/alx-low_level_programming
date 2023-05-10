cad857998b991dca4f737ef798959ee99156b418
Flora Frimpong-Boateng <florafrimps@gmail.com>
2023-05-10 11:31:47 -0700

    Inception. Is it possible

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,21 +3,20 @@
 /**
  * is_prime_number - checks if a number is prime
  * @n: integer to check
- *
- * Return: 1 if n is prime, 0 otherwise
+ * @divisor: divisor to check n against
+ * Return: 1 if prime, 0 otherwise
  */
-int is_prime_number(int n)
+   
+int is_prime_helper(int n, int divisor)
 {
-int i;
-
+/* base cases */
 if (n <= 1)
 return (0);
-
-for (i = 2; i <= n / 2; i++)
-{
-if (n % i == 0)
-return (0);
-}
-
+if (divisor >= n)
 return (1);
+/* check if n is divisible by divisor */
+if (n % divisor == 0)
+return (0);
+
+return (is_prime_number(n, divisor + 1));
 }
