1ff89b1c23ae8e0ccfa8f5bc0d09ea03834932bc
Flora Frimpong-Boateng <florafrimps@gmail.com>
2023-05-17 10:34:32 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,23 +1,47 @@
+#include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
+/**
+ * str_concat - concatenate two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to the concatenated string
+ */
 char *str_concat(char *s1, char *s2)
 {
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+char *concat;
+int i, ci;
 
-    int len1 = strlen(s1);
-    int len2 = strlen(s2);
-    int totalLen = len1 + len2;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-    char *result = malloc((totalLen + 1) * sizeof(char));
-    if (result == NULL)
-        return NULL;
+i = ci = 0;
+while (s1[i] != '\0')
+i++;
+while (s2[ci] != '\0')
+ci++;
 
-    strcpy(result, s1);
-    strcat(result, s2);
+concat = malloc(sizeof(char) * (i + ci + 1));
+if (concat == NULL)
+return (NULL);
 
-    return result;
+i = ci = 0;
+while (s1[i] != '\0')
+{
+concat[i] = s1[i];
+i++;
 }
+
+while (s2[ci] != '\0')
+{
+concat[i] = s2[ci];
+i++;
+ci++;
+}
+
+concat[i] = '\0';
+return (concat);
+}
+
