14bddde708e88bbc214bb728b5a1abdca0ebacac
Florafrimps <florafrimps@gmail.com>
2023-06-12 10:29:58 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <fcntl.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 char *create_buffer(char *file);
 void close_file(int fd);
